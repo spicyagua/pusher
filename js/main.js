6c0fdf4337b6bf26142b85acb6c173df855789e6
unknown <bill.scott@gmail.com>
2014-08-06 12:32:57 -0700

    Try on bluehost

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 var PT1 = PT1 || {};
 
 PT1.app = (function() {
+
+  var pusher;
+  var channel;
+
   var _main = function() {
     console.log("PT1 main");
-    var pusher = new Pusher("34690");
-    var channel = pusher.subscribe('my-channel');
+
+    Pusher.log = function(message) {
+      console.log(message);
+    };
+
+    pusher = new Pusher('95c1ee6cc09dfd0efb14');
+    channel = pusher.subscribe('test_channel');
+    channel.bind('my_event', function(data) {
+      console.log(data);
+      console.log("Got one: " + data.message);
+    });
+
+/*
+    var pusher = new Pusher("95c1ee6cc09dfd0efb14");
+    var channel = pusher.subscribe('test_channel');
     channel.bind('my-event', function(data) {
       alert('An event was triggered with message: ' + data.message);
-    });    
+      console.log("Got one");
+    });
+*/
   };
   return {
     main: _main
---
+++ test1.php
@@ -0,0 +1,14 @@
+<?php
+
+  $start = time();
+  echo "a: " . (time()-$start) . "<br>";
+  require("lib/Pusher.php");
+
+  echo "b: " . (time()-$start) . "<br>";
+  $pusher = new Pusher("95c1ee6cc09dfd0efb14", "e92f4343dc6f2034b020", "34690");
+  echo "c: " . (time()-$start) . "<br>";
+  $pusher->trigger("test_channel", "my-event", array( "message" => "hello world"));
+  echo "d: " . (time()-$start) . "<br>";
+
+
+?>
