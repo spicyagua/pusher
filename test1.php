de57230488a35a701525d3d17afd607e863223c1
unknown <bill.scott@gmail.com>
2014-08-06 12:35:40 -0700

    fix test

--- test1.php
+++ test1.php
@@ -7,7 +7,7 @@
   echo "b: " . (time()-$start) . "<br>";
   $pusher = new Pusher("95c1ee6cc09dfd0efb14", "e92f4343dc6f2034b020", "34690");
   echo "c: " . (time()-$start) . "<br>";
-  $pusher->trigger("test_channel", "my-event", array( "message" => "hello world"));
+  $pusher->trigger("test_channel", "my_event", array( "message" => "hello world"));
   echo "d: " . (time()-$start) . "<br>";
 
 
